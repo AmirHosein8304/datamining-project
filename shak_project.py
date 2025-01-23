8b042e7924886bc886adb0b68c76b6436b385dda
AmirHosein <ahtavakkoli96@gmail.com>
2025-01-23 15:48:26 +0330

    seccond try on 4th Bahman

--- shak_project.py
+++ shak_project.py
@@ -8,6 +8,7 @@ store_data = pd.read_csv('store.csv')
 store_data.drop(['RetailType','RivalOpeningMonth','RivalEntryYear'],axis=1,inplace=True)
 training_data = pd.read_csv('train.csv')
 Data = pd.merge(training_data, store_data, on='Store_id')
+Data.drop('NumberOfCustomers',axis=1,inplace=True)
 Data['DistanceToRivalStore'].fillna(Data['DistanceToRivalStore'].median(), inplace=True)
 Data.fillna(0, inplace=True)
 Data['Date'] = pd.to_datetime(Data['Date'])
@@ -16,5 +17,5 @@ Data['Month'] = Data['Date'].dt.month
 Data['Day'] = Data['Date'].dt.day
 Data['WeekOfYear'] = Data['Date'].dt.isocalendar().week
 standardizer = StandardScaler()
-standardizer.fit_transform(Data.drop(['Sales','Date'], axis=1))
+standardizer.fit_transform(Data.drop(['Stock variety','ContinuousBogoMonths','Date'], axis=1))
 X_train, X_test, y_train, y_test = train_test_split(Data.drop('Sales', axis=1), Data['Sales'], test_size=0.2, random_state=42)
