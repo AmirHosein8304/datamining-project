08a4a04f6c865cfc1361ad24a462a1c294286b88
AmirHosein <ahtavakkoli96@gmail.com>
2025-02-05 18:45:21 +0330

    last update

--- seccond-part.py
+++ seccond-part.py
@@ -1,9 +1,13 @@
+import time
+s = time.time()
+from numpy import nan
 import pandas as pd
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from sentence_transformers import SentenceTransformer
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import f1_score
@@ -16,5 +20,28 @@ def preprocess_text(text):
     return ' '.join(tokens)
 
 Data = pd.read_csv('train_sentiment.csv')
+Data['rating'].replace('|', nan, inplace=True)
+Data['rating'] = pd.to_numeric(Data['rating'], errors='coerce')
+Data['rating'].fillna(Data['rating'].mean() ,inplace=True)
+Data.loc[Data['rating'] >= 3.5, 'rating'] = 6
+Data.loc[Data['rating'] < 3.5,'rating'] = -1
 Data['cleaned_review'] = Data['review'].apply(preprocess_text)
-
+vectorizer = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+Data['vector'] = Data['cleaned_review'].apply(lambda x: vectorizer.encode(x))
+X_train, X_test, y_train, y_test = train_test_split(Data.drop('rating',axis=1), Data['rating'], test_size=0.2, random_state=42)
+log_reg = LogisticRegression()
+rnd_for = RandomForestClassifier()
+knn = KNeighborsClassifier()
+log_reg.fit(list(X_train['vector']), y_train)
+print('LogisticRegression score:',f1_score(y_test, log_reg.predict(list(X_test['vector'])), average='weighted'))
+rnd_for_hyp = {'n_estimators': [10, 50, 100, 200],'max_depth': [5, 10,15]}
+rnd_for_grid = GridSearchCV(rnd_for, rnd_for_hyp, cv=5,scoring='f1_weighted',verbose=4)
+rnd_fit_model = rnd_for_grid.fit(list(X_train['vector']), y_train)
+print('Best parameters for RandomForestRegressor: ', rnd_fit_model.best_params_)
+print('Best score for RandomForestRegressor: ', rnd_fit_model.best_score_)
+knn_hyp = {'n_neighbors': [3, 5, 7, 9, 11]}
+knn_grid = GridSearchCV(knn, knn_hyp, cv=5, scoring='f1_weighted', verbose=4)
+knn_fit_model = knn_grid.fit(list(X_train['vector']), y_train)
+print('Best parameters for KNeighborsClassifier: ', knn_fit_model.best_params_)
+print('Best score for KNeighborsClassifier: ', knn_fit_model.best_score_)
+print(time.time()-s)
--- shak_project.py
+++ shak_project.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,7 +10,8 @@ store_data.drop(['RetailType','RivalOpeningMonth','RivalEntryYear','ContinuousBo
 training_data = pd.read_csv('train.csv')
 Data = pd.merge(training_data, store_data, on='Store_id')
 Data.drop('NumberOfCustomers',axis=1,inplace=True)
-Data['DistanceToRivalStore'].fillna(Data['DistanceToRivalStore'].median(), inplace=True)
+median_distance = store_data['DistanceToRivalStore'].median() 
+store_data['DistanceToRivalStore'] = store_data['DistanceToRivalStore'].fillna(median_distance)
 Data.fillna(0, inplace=True)
 Data['Date'] = pd.to_datetime(Data['Date'])
 Data['Year'] = Data['Date'].dt.year
@@ -27,5 +29,6 @@ linear_mod = LinearRegression()
 random_forest = RandomForestRegressor(n_estimators=100)
 linear_mod.fit(X_train, y_train)
 random_forest.fit(X_train, y_train)
-print('Linear Regression accuracy:',mean_squared_error(y_test, linear_mod.predict(X_test)))
-print('Random Forest accuracy:', mean_squared_error(y_test, random_forest.predict(X_test)))
+'''print('Linear Regression accuracy:',mean_squared_error(y_test, linear_mod.predict(X_test)))
+print('Random Forest accuracy:', mean_squared_error(y_test, random_forest.predict(X_test)))'''
+print(Data.drop('Sales').columns.tolist()[np.unravel_index(np.argmax(random_forest.feature_importances_),random_forest.feature_importances_.shape)[0]])
